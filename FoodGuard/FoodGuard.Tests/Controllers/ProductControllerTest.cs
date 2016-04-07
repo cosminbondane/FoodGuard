ea6a5e9f1c3368dc21d0e2bd4e71d0356803dcf7
Florentina - Lucia Caputa <fcaputa@RONET1.LOCAL>
2016-04-07 20:42:53 +0300

    Add App.config files and unit test

---
+++ FoodGuard/FoodGuard.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using FoodGuard.WebApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodGuard.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void GetCartTypes()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+            var result = controller.GetProducts();
+
+            //// Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
